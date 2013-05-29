42154975967fb3acaebe4a581d169285653abd1a
D. Stuart Freeman <stuart.freeman@et.gatech.edu>
2013-05-29 15:39:53 -0400

    add uninstaller, create start script

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,3 +1,4 @@
 Install-ChocolateyZipPackage 'etherpad-lite' 'http://github.com/ether/etherpad-lite/archive/1.2.91.zip' "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-# Install-ChocolateyPath "$(Split-Path -parent $MyInvocation.MyCommand.Definition)\elasticsearch-0.90.0\bin"
+echo "cd $(Split-Path -parent $MyInvocation.MyCommand.Definition)\etherpad-lite-1.2.1
+call start.bat" | set-content "$env:chocolateyinstall\bin\etherpad-lite.bat"
---
+++ tools/chocolateyUninstall.ps1
@@ -0,0 +1,1 @@
+Remove-Item $env:chocolateyinstall\bin\etherpad-lite.bat
